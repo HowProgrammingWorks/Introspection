9b93b0c60135ad2941141674cad0e1f3bc185b0c
tshemsedinov <timur.shemsedinov@gmail.com>
2019-05-26 00:58:23 +0300

    Add symbols enumeration example

--- JavaScript/d-descriptors.js
+++ JavaScript/d-descriptors.js
@@ -1,19 +1,27 @@
 'use strict';
 
-const object = { a: 1, b: 2, c: 3 };
+const object = {
+  a: 1,
+  b: 2,
+  c: 3,
+  [Symbol('d')]: 4,
+  [Symbol('e')]: 5,
+};
 
 const array = [1, 2, 3];
 
-const objectDescriptors = Object.getOwnPropertyDescriptors(object);
-const arrayDescriptors = Object.getOwnPropertyDescriptors(array);
-
 const objectProperties = Object
   .getOwnPropertyNames(object)
-  .map(prop => ({ prop, ...objectDescriptors[prop] }));
+  .map(key => ({ key, ...Object.getOwnPropertyDescriptor(object, key) }));
+
+const objectSymbolProperties = Object
+  .getOwnPropertySymbols(object)
+  .map(key => ({ key, ...Object.getOwnPropertyDescriptor(object, key) }));
 
 const arrayProperties = Object
   .getOwnPropertyNames(array)
-  .map(prop => ({ prop, ...arrayDescriptors[prop] }));
+  .map(key => ({ key, ...Object.getOwnPropertyDescriptor(array, key) }));
 
 console.table(objectProperties);
+console.table(objectSymbolProperties);
 console.table(arrayProperties);
